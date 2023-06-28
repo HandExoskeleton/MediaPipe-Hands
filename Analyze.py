78bea244f03c11981a8f797def21dd49d2423799
chrislee8684 <kanghyuk.lee@cooper.edu>
2023-06-27 20:57:16 -0400

    updated code

--- Analyze.py
+++ Analyze.py
@@ -1,48 +1,205 @@
 import csv
 import numpy as np
+import math
+import os
+import matplotlib.pyplot as plt
 
+def process_coord(projName, csv_file):
 
-# Create a 1D matrix (NumPy array) with 21 columns
-matrix = np.zeros(21)
+    # Open the CSV file for reading
+    file = open(csv_file, 'r')
 
-# Prompt user to enter file
-csv_file = input("Enter file name: ")
+    # Create a CSV reader object
+    reader = csv.reader(file)
 
-# Open the CSV file for reading
-file = open(csv_file, 'r')
+    ### Generate Distance CSV File and Plot ###
 
-# Create a CSV reader object
-reader = csv.reader(file)
+    # 2D array to hold distance coordinates and time steps
+    totalDistCoord = []  # accumulates -> used for Total Distance Plot
 
-# Skip the header row
-next(reader)
+    # Skip the header row
+    next(reader)
 
-# Initialize previous row
-previous_row = None
+    # Initialize previous row
+    previous_row = None
 
-# Read and process each row
-for row in reader:
-    # Check if previous row exists
-    if previous_row is not None:
-        # Subtract respective values from each cell tuple in the current row
-        result = []
-        for curr_cell, prev_cell in zip(row, previous_row):
-            curr_tuple = tuple(map(int, curr_cell.strip('()').split(',')))
-            prev_tuple = tuple(map(int, prev_cell.strip('()').split(',')))
+    # Open a new CSV file for writing
+    output_file = open(os.path.join(projName, projName + "_Distance.csv"), 'w', newline='')
+    writer = csv.writer(output_file)
 
-            # Perform subtraction based on tuple length
-            if len(curr_tuple) == len(prev_tuple):
-                diff = tuple(curr_val - prev_val for curr_val, prev_val in zip(curr_tuple, prev_tuple))
+    # Write header row
+    landmark_positions = []
+    for i in range(21):
+        landmark_positions.extend(['Landmark ' + str(i + 1)])
+    landmark_positions.extend(["Time"])
+    writer.writerow(landmark_positions)
+
+    # Read and process each row
+    for row in reader:
+        # Check if previous row exists
+        if previous_row is not None:
+            # Subtract respective values from each cell in the current row
+            result = []
+            distance = []
+            for curr_cell, prev_cell in zip(row, previous_row):
+                curr_values = curr_cell.strip('()').split(', ')
+                prev_values = prev_cell.strip('()').split(', ')
+
+                # Convert values to integers or floats
+                curr_values = [int(val) if val.isdigit() else float(val) for val in curr_values]
+                prev_values = [int(val) if val.isdigit() else float(val) for val in prev_values]
+
+                # Perform subtraction based on value count
+                if len(curr_values) == len(prev_values):
+                    diff = [curr_val - prev_val for curr_val, prev_val in zip(curr_values, prev_values)]
+                else:
+                    diff = None  # Handle the case when value counts are different
+
+                result.append(diff)  # Difference of values between rows
+
+            for index, cell in enumerate(result):  # only apply distance formula to coordinates (not time)
+                if index < 21:
+                    distance.append(float(math.sqrt(cell[0] ** 2 + cell[1] ** 2 + cell[2] ** 2)))
+                else:
+                    distance.append(float(str(cell)[1:-1]))  # time difference
+
+            # Write the distance to the CSV file
+            writer.writerow(distance)
+
+            # Add row to distCoord 2D array
+            if len(totalDistCoord) == 0:  # Check if array is empty -> do not add previous row
+                totalDistCoord.append(distance)
             else:
-                diff = None  # Handle the case when tuple lengths are different
+                totalDistCoord.append([x + y for x, y in zip(distance, totalDistCoord[-1])])  # Total distance accumulates
 
-            result.append(diff)
+        # Assign the current row as the previous row for the next iteration
+        previous_row = row
 
-        # Print the result
-        print(result)
+    # Close the files
+    file.close()
+    output_file.close()
 
-    # Assign the current row as the previous row for the next iteration
-    previous_row = row
+    # Plot Total Distance Graph
+    plt.figure(figsize=(10, 6))  # Adjust the width and height as desired
 
-# Close the file
-file.close()
+    for i in range(21):
+        plt.plot(list(zip(*totalDistCoord))[21], list(zip(*totalDistCoord))[i], label="Landmark " + str(i + 1))
+
+    plt.xlabel('Total Time (milli-seconds)')
+    plt.ylabel('Total Distance')
+    plt.title('Total Distance per Landmark')
+    plt.legend(loc='upper left', bbox_to_anchor=(1.02, 1), borderaxespad=0.)
+    plt.subplots_adjust(right=0.7)
+    save_path = os.path.join(projName, projName + "_TotalDistancePlot.png")
+    plt.savefig(save_path)  # Save plot
+    plt.show()
+
+    ### Generate Velocity CSV File and Plot ###
+
+    # Calculate velocity
+    time = list(zip(*totalDistCoord))[21]  # Extract time values
+    total_distance = list(zip(*totalDistCoord))[:21]  # Extract total distance values for each landmark
+
+    velocity = []
+    for distances in total_distance:
+        landmark_velocity = [distances[0]]  # Starting velocity is the first distance value
+        for i in range(1, len(distances)):
+            delta_distance = distances[i] - distances[i - 1]  # Calculate change in distance
+            delta_time = time[i] - time[i - 1]  # Calculate change in time
+            landmark_velocity.append(delta_distance / delta_time)  # Calculate velocity and append to list
+        velocity.append(landmark_velocity)  # Append velocity list for each landmark
+
+    # Open a new CSV file for writing velocity
+    velocity_output_file = open(os.path.join(projName, projName + "_Velocity.csv"), 'w', newline='')
+    velocity_writer = csv.writer(velocity_output_file)
+
+    # Write header row
+    velocity_header = []
+    for i in range(21):
+        velocity_header.append("Landmark " + str(i + 1))
+    velocity_header.extend(["Time"])
+    velocity_writer.writerow(velocity_header)
+
+    # Write velocity data to the CSV file
+    for i in range(len(time)):
+        velocity_row = []
+        for j in range(21):
+            if j < len(velocity):  # Add this condition to check if j is within the valid range
+                velocity_row.append(velocity[j][i])
+            else:
+                print("Index out of range: j =", j)
+        velocity_row.append(time[i])
+        velocity_writer.writerow(velocity_row)
+
+    # Close the file
+    velocity_output_file.close()
+
+    # Plot Velocity Graph
+    plt.figure(figsize=(10, 6))  # Adjust the width and height as desired
+
+    for i in range(21):
+        plt.plot(time, velocity[i], label="Landmark " + str(i + 1))
+
+    plt.xlabel('Time (milli-seconds)')
+    plt.ylabel('Velocity')
+    plt.title('Velocity per Landmark')
+    plt.legend(loc='upper left', bbox_to_anchor=(1.02, 1), borderaxespad=0.)
+    plt.subplots_adjust(right=0.7)
+    save_path = os.path.join(projName, projName + "_VelocityPlot.png")
+    plt.savefig(save_path)  # Save plot
+    plt.show()
+
+    ### Generate Acceleration CSV File and Plot ###
+
+    # Calculate acceleration
+    acceleration = []
+    for velocities in velocity:
+        landmark_acceleration = [velocities[0]]  # Starting acceleration is the first velocity value
+        for i in range(1, len(velocities)):
+            delta_velocity = velocities[i] - velocities[i - 1]  # Calculate change in velocity
+            delta_time = time[i] - time[i - 1]  # Calculate change in time
+            landmark_acceleration.append(delta_velocity / delta_time)  # Calculate acceleration and append to list
+        acceleration.append(landmark_acceleration)  # Append acceleration list for each landmark
+
+    # Open a new CSV file for writing acceleration
+    acceleration_output_file = open(os.path.join(projName, projName + "_Acceleration.csv"), 'w', newline='')
+    acceleration_writer = csv.writer(acceleration_output_file)
+
+    # Write header row
+    acceleration_header = []
+    for i in range(21):
+        acceleration_header.append("Landmark " + str(i + 1))
+    acceleration_header.extend(["Time"])
+    acceleration_writer.writerow(acceleration_header)
+
+    # Write acceleration data to the CSV file
+    for i in range(len(time)):
+        acceleration_row = []
+        for j in range(21):
+            if j < len(acceleration):  # Add this condition to check if j is within the valid range
+                acceleration_row.append(acceleration[j][i])
+            else:
+                print("Index out of range: j =", j)
+        acceleration_row.append(time[i])
+        acceleration_writer.writerow(acceleration_row)
+
+    # Close the file
+    acceleration_output_file.close()
+
+    # Plot Acceleration Graph
+    plt.figure(figsize=(10, 6))  # Adjust the width and height as desired
+
+    for i in range(21):
+        plt.plot(time, acceleration[i], label="Landmark " + str(i + 1))
+
+    plt.xlabel('Time (milli-seconds)')
+    plt.ylabel('Acceleration')
+    plt.title('Acceleration per Landmark')
+    plt.legend(loc='upper left', bbox_to_anchor=(1.02, 1), borderaxespad=0.)
+    plt.subplots_adjust(right=0.7)
+    save_path = os.path.join(projName, projName + "_AccelerationPlot.png")
+    plt.savefig(save_path)  # Save plot
+    plt.show()
+
+# Run analysis
+# process_coord("sample", os.path.join("sample", "sample" + "_Coordinates.csv"))
--- Capture.py
+++ Capture.py
@@ -2,6 +2,9 @@ import os
 import cv2
 import mediapipe as mp
 import csv
+import numpy as np
+from Analyze import process_coord
+import time
 
 # Prompt user for file name
 projName = input("Enter project name: ")
@@ -10,13 +13,14 @@ projName = input("Enter project name: ")
 os.makedirs(projName, exist_ok=True)
 
 # Open the file for writing
-file = open(os.path.join(projName, projName + ".csv"), 'w', newline='')
+file = open(os.path.join(projName, projName + "_Coordinates.csv"), 'w', newline='')
 writer = csv.writer(file)
 
 # Define columns
 landmark_positions = []
 for i in range(21):
     landmark_positions.extend(['Landmark ' + str(i+1)])
+landmark_positions.extend(["Time"])
 writer.writerow(landmark_positions)
 
 # Initialize MediaPipe Hands
@@ -27,13 +31,20 @@ mp_hands = mp.solutions.hands
 # Open video capture
 cap = cv2.VideoCapture(0)
 
+# Record start time
+startTime = time.time()
+
 # Get the video width and height
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# Define video writer
+# Define video writers
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-output_video = cv2.VideoWriter(os.path.join(projName, projName + '_LandmarkFootage.mp4'), fourcc, 20.0, (width, height))
+landmarks_video = cv2.VideoWriter(os.path.join(projName, projName + '_LandmarksOnly.mp4'), fourcc, 20.0, (width, height))
+velocity_video = cv2.VideoWriter(os.path.join(projName, projName + '_VelocityField.mp4'), fourcc, 20.0, (width, height))
+
+# Initialize previous landmark positions
+previous_landmarks = None
 
 while True:
     # Read frame from video capture
@@ -50,12 +61,16 @@ while True:
     # Process frame with MediaPipe Hands
     results = hands.process(frame_rgb)
 
+    # Create a black background image
+    black_frame = np.zeros_like(frame)
+
     # Check if hand landmarks are detected
     if results.multi_hand_landmarks:
         # Iterate over each detected hand
         for hand_landmarks in results.multi_hand_landmarks:
             # Draw landmarks on the frame
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            mp_drawing.draw_landmarks(black_frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
             # Array for storing each position instance (row)
             landmark_row = []
@@ -73,11 +88,35 @@ while True:
                 # Display position as text
                 cv2.putText(frame, f"Landmark {idx+1}: ({x}, {y}, {z})", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
+            # Append timestamp to the landmark_row
+            landmark_row.append(time.time()-startTime) #current time - start time for calibration
+
             # Write row to csv file
             writer.writerow(landmark_row)
 
-    # Write frame to output video
-    output_video.write(frame)
+            # Calculate velocity field
+            if previous_landmarks is not None:
+                velocity_field = []
+                for prev_landmark, curr_landmark in zip(previous_landmarks, landmark_row[:-1]):
+                    prev_x, prev_y, _ = prev_landmark
+                    curr_x, curr_y, _ = curr_landmark
+                    delta_x = curr_x - prev_x
+                    delta_y = curr_y - prev_y
+                    velocity_field.append((delta_x, delta_y))
+
+                # Draw velocity field on the black frame
+                for velocity_vector, curr_landmark in zip(velocity_field, landmark_row[:-1]):
+                    curr_x, curr_y, _ = curr_landmark
+                    delta_x, delta_y = velocity_vector
+                    end_point = (curr_x + delta_x, curr_y + delta_y)
+                    cv2.arrowedLine(black_frame, (curr_x, curr_y), end_point, (0, 0, 255), 2)
+
+            # Update previous_landmarks
+            previous_landmarks = landmark_row[:-1]
+
+    # Write frame to output videos
+    landmarks_video.write(black_frame)
+    velocity_video.write(frame)
 
     # Show the frame
     cv2.imshow('MediaPipe Hands', frame)
@@ -86,9 +125,13 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Run analysis
+process_coord(projName, os.path.join(projName, projName + "_Coordinates.csv"))  #passing it as a path rather than object
+
 # Release resources
 cap.release()
-output_video.release()
+landmarks_video.release()
+velocity_video.release()
 cv2.destroyAllWindows()
 hands.close()
 
